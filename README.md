a092c1a721b8b31b08c2c1e4217452d83b94f245
Daniel Fazzioni <daniel.fazzioni@gmail.com>
2023-02-02 08:59:34 -0300

    Adicionado várias tabelas

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 </div>
 
+[MODELAGEM DE CLASSE](https://lucid.app/lucidchart/ce115dfe-d00c-4188-aa1a-a6653f41fbf5/edit?invitationId=inv_601fced8-5f9e-4082-bf69-d092d526e6ce&page=0_0#)
--- app/models.py
+++ app/models.py
@@ -8,3 +8,27 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+class Events(db.Model):
+    code = db.Column(db.Integer, primary_key=True)
+    code_cam = db.Column(db.Integer)
+    timestamp = db.Column(db.String(30))
+    recepients_method = db.Column(db.String(100))
+    recepients_adress = db.Column(db.String(100))
+    send = db.Column(db.Integer)
+    message = db.Column(db.String(1000))
+
+class Cams(db.Model):
+    code = db.Column(db.Integer, primary_key=True)
+    client_code = db.Column(db.Integer)
+    backup_image = db.Column(db.Integer)
+    backup_img_days = db.Column(db.Integer)
+    type_event = db.Column(db.String(2))
+    label = db.Column(db.String(100))
+
+class recepientes(db.Model):
+    code = db.Column(db.Integer, primary_key=True)
+    code_cam = db.Column(db.Integer)
+    method = db.Column(db.String(100))
+    adress = db.Column(db.String(100))
